7c3d12b6b438d5347e986ce2344d8edd974e6e40
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-09-05 21:10:17 -0400

    Lecture done

--- api/server.js
+++ api/server.js
@@ -1,21 +1,47 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+const connectSessionKnex = require('connect-session-knex')
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const db = require('../database/dbConfig')
 
 const server = express();
+const KnexSessionStore = connectSessionKnex(session)
+
+const sessionConfig = {
+  name: "notsession", // default is connect.sid
+  // This should not be hardcoded. This should be in an env variable
+  secret: "monsoon demons stole my teacher",
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day in milliseconds
+    secure: false // set to TRUE in production -- only set cookies over https. Server will not send back a cookie over http.
+  }, 
+  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+  resave: false,
+  saveUninitialized: false,
+  // Where do we store our sessions? defaults to server
+  store: new KnexSessionStore({
+    knex: db,
+    tablename: 'sessions',
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60 * 24 * 1
+  })
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
 });
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -11,6 +11,9 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
+      //create a session (store locally or in db)
+      //send back cookie that corresponds to session
+      req.session.user = saved.username
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -25,8 +28,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        //create a session (store locally or in db)
+        //send back cookie that corresponds to session
+        req.session.user = user.username
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}! Have a cookie!`,
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +42,17 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.json({message: "You can checkout but you can't leave: ", err})
+      } else {
+        res.json({message: "You have logged out"})
+      }
+    })
+  }
+})
 
 module.exports = router;
